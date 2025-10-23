02a4b79b69790d9bd117fa84caac6ab03dcccbae
Diana Santos <diana@Diana.local>
2025-10-22 18:12:23 -0600

    clase

---
+++ src/BilleteraParque.java
@@ -0,0 +1,53 @@
+public class BilleteraParque {
+    private int tickets;
+    private static boolean festivo = false;
+
+    
+
+    public void BilleteraParque(){
+        tickets = 0;
+    }
+
+    public BilleteraParque(int tickets) {
+        if(tickets >= 0){
+            tickets = tickets;
+        }else{
+            tickets = 0;
+        }
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
+
+    public static boolean getFestivo() {
+        return festivo;
+    }
+
+    public static void setFestivo() {
+        festivo = !festivo;
+    }
+
+
+    public void agregarTickets(int cantidadTickets){
+        if(cantidadTickets >= 0){
+            tickets += cantidadTickets;
+        }
+    }
+
+
+    public boolean removerTickets(int cantidadTickets){
+        if (cantidadTickets >= 0 && tickets >= cantidadTickets) {
+            tickets -= cantidadTickets;
+            return true;
+            
+        } else {
+            return false;
+        }
+    }
+
+}
